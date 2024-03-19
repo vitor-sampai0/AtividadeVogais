507f1306a7d615213769cd4b9f84e4f10fc0bb78
vitor-sampaio <vitorsampaio.tds2.senai@gmail.com>
2024-03-19 16:58:57 -0300

    atividade completa

---
+++ atividadevogal.js
@@ -0,0 +1,29 @@
+let consoante = ["b", "c", "d", "f", "g", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "e", "z"]
+let vogais = ['a', 'e', 'i', 'o', 'u'];
+let texto = "vitor sampaio e super daora";
+
+let arrayletras = texto.split("");
+
+
+
+let arrayvogais = [];
+
+let arrayconsoante = []
+
+for(let i = 0; i < arrayletras.length; i++){
+    if(vogais.includes(arrayletras[i])){
+        arrayvogais.push(arrayletras[i]);
+    }
+}
+
+for(let f = 0; f < arrayletras.length; f++){
+    if(consoante.includes(arrayletras[f])){
+        arrayconsoante.push(arrayletras[f]);
+    }
+}
+
+console.log("texto original:", texto)
+console.log("vogais encontradas:", arrayvogais.toString());
+console.log("total de vogais:" + arrayvogais.length)
+console.log("consoantes encontradas:", arrayconsoante.toString());
+console.log("total de consoantes:" + arrayconsoante.length)
